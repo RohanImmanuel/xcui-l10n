a4d3497610575a7410736b4edb80006f5816c133
RohanImmanuel <i25.rohan@gmail.com>
2026-04-01 11:22:00 +0100

    Add app entry point and root navigation

---
+++ SimpleSettings/App/SimpleSettingsApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct SimpleSettingsApp: App {
+    @StateObject private var settings = AppSettings()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(settings)
+        }
+    }
+}
